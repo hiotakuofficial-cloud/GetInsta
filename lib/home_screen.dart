949c20e9920ea20d19a1d839cdf5295e4cc5582a
root <root@localhost.localdomain>
2025-11-27 18:33:39 +0000

    Fix class structure - remove extra closing brace after initState

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -62,8 +62,6 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
       Permission.notification,
     ].request();
   }
-  }
-
   @override
   void dispose() {
     _searchController.dispose();
